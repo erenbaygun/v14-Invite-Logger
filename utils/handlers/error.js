751ce2cbf000ccb645066f0284fbd120b10d28f3
Eren Baygun <eren@baygun.net>
2022-07-18 21:02:05 +0300

    hata mesajları türkçeye çevrildi

--- utils/handlers/error.js
+++ utils/handlers/error.js
@@ -1,22 +1,22 @@
 module.exports = async (client) => {
 
     process.on('unhandledRejection', err => {
-        if(err) {
+        if (err) {
             if (err.stack.includes('An invalid token was provided.')) {
-                return client.logger.error('Bad token see config.js for set the token')
+                return client.logger.error('Geçersiz token, config.js dosyasını kontrol et.')
             } else if (err.stack.includes('Missing Permissions')) {
-                return client.logger.error('Permission Error')
+                return client.logger.error('Botun izinlerini yetersiz')
             } else {
                 return client.logger.error(err.stack)
             }
         }
     });
-   
-    process.on('uncaughtException', err =>{
+
+    process.on('uncaughtException', err => {
         if (err.stack.includes('Promise { <pending> }')) return;
         return client.logger.error(err.stack)
     });
-   
+
     process.on('warning', (err) => {
         client.logger.error(err.stack)
     })
