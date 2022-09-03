e6d41ce53033aa2dc068e162a731ce53965284f8
Eren Baygun <eren@baygun.net>
2022-09-04 00:06:08 +0300

    fix

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -1,13 +1,13 @@
 const inviteDB = require("../database/schemas/invite")
 
 module.exports = async (client, oldGuild, newGuild) => {
-    if (oldGuild.vanityURLCode !== newVanity.vanityURLCode) {
+    if (oldGuild.vanityURLCode !== newGuild.vanityURLCode) {
         await inviteDB.findOneAndUpdate({ guildId: newGuild.id },
             {
                 $set: {
                     vanity: {
-                        code: newVanity.code,
-                        uses: vanity.uses
+                        code: newGuild.vanityURLCode,
+                        uses: newGuild.vanityURLUses
                     }
                 }
             }
